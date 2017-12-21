a353fa3593804f79bf7e787c0a7df3521386e277
jlippold <jlippold@datacation.com>
2017-12-21 12:34:10 -0500

    simple tweak list

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,67 @@
+var vm;
+var tweakList;
+
 $(document).ready(function () {
-    $.getJSON("tweaks.json", function (data) {
-        console.log(data);
+
+    var TweakList = Vue.extend({
+        template: "#tweaklist-template",
+        data: function () {
+            return {
+                data: {
+                    searchTerm: "",
+                    iOSVersionIndex: 0,
+                    allowedCategories: [],
+                    iOSVersions: [],
+                    allowedDevices: [],
+                    packages: []
+                }
+            };
+        },
+        beforeMount: function () {
+        },
+        mounted: function () {
+            var c = this;
+            this.fetch();
+        },
+        computed: {
+            filteredPackages: function() {
+                var data = this.data;
+                var iOSVersion = data.iOSVersions[data.iOSVersionIndex];
+                var searchTerm = data.searchTerm.toLowerCase();
+                return data.packages.filter(function(package) {
+                    if (searchTerm == "") {
+                        return true;
+                    }
+                    return (
+                        package.name.toLowerCase().indexOf(searchTerm) > -1 ||
+                        package.depiction.toLowerCase().indexOf(searchTerm) > -1
+                    );
+                });
+            }
+        },
+        methods: {
+            selectOSFilter: function (event, index) {
+                this.data.iOSVersionIndex = index;
+            },
+            fetch: function (done) {
+                var c = this;
+                $.getJSON("tweaks.json", function (data) {
+                    c.data.allowedCategories = data.allowedCategories.slice();
+                    c.data.iOSVersions = data.iOSVersions.slice();
+                    c.data.allowedDevices = data.allowedDevices.slice();
+                    c.data.packages = data.packages.slice();
+                    if (done) { done(); }
+                });
+            }
+        }
+    });
+
+    vm = new Vue({
+        el: "#app",
+        data: {},
+        components: {
+            tweaklist: TweakList
+        }
     });
 
     $(".input-group-btn .dropdown-menu li a").click(function () {
