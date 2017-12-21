4f7c7f795cc9305b0e490abbf1342b82025a895f
jlippold <jlippold@datacation.com>
2017-12-21 13:22:07 -0500

    basic web view with filters

--- docs/index.js
+++ docs/index.js
@@ -24,11 +24,12 @@ $(document).ready(function () {
             this.fetch();
         },
         computed: {
-            filteredPackages: function() {
+            
+            filteredPackages: function () {
                 var data = this.data;
                 var iOSVersion = data.iOSVersions[data.iOSVersionIndex];
                 var searchTerm = data.searchTerm.toLowerCase();
-                return data.packages.filter(function(package) {
+                var filtered = data.packages.filter(function (package) {
                     if (searchTerm == "") {
                         return true;
                     }
@@ -36,6 +37,56 @@ $(document).ready(function () {
                         package.name.toLowerCase().indexOf(searchTerm) > -1 ||
                         package.depiction.toLowerCase().indexOf(searchTerm) > -1
                     );
+                });
+
+                return filtered.map(function (package) {
+                    package.iOSVersion = iOSVersion;
+                    package.outcome = {
+                        status: "",
+                        percentage: 100,
+                        counts: {
+                            good: 0,
+                            bad: 0
+                        }
+                    };
+
+                    if (package.status.hasOwnProperty("good")) {
+                        package.status.good.forEach(function (report) {
+                            if (report.iOS == iOSVersion && report.tweakVersion == package.latest) {
+                                package.outcome.counts.good += 1;
+                            }
+                        });
+                    }
+                    if (package.status.hasOwnProperty("bad")) {
+                        package.status.bad.forEach(function (report) {
+                            if (report.iOS == iOSVersion && report.tweakVersion == package.latest) {
+                                package.outcome.counts.bad += 1;
+                            }
+                        });
+                    }
+                    
+                    package.outcome.counts.total = package.outcome.counts.good + package.outcome.counts.bad;
+                    package.outcome.percentage = package.outcome.counts.total == 0 ? 0 : Math.floor((package.outcome.counts.good / package.outcome.counts.total) * 100);
+
+                    package.outcome.status = "Not working";
+
+                    if (package.outcome.percentage == 0) {
+                        package.outcome.status = "Unknown";
+                    }
+                    if (package.outcome.percentage > 40) {
+                        package.outcome.status = "Likely working";
+                    }
+                    if (package.outcome.percentage > 75) {
+                        package.outcome.status = "Working";
+                    }
+
+                    package.outcome.classObject = {
+                        "label-success": (package.outcome.status == "Working"),
+                        "label-danger": (package.outcome.status == "Not working"),
+                        "label-warning": (package.outcome.status == "Likely working"),
+                        "label-default": (package.outcome.status == "Unknown")
+                    };
+                    return package;
                 });
             }
         },
