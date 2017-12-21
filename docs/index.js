dc2acfbfedab8eaccd19e731355eeac7c735ae26
jlippold <jlippold@datacation.com>
2017-12-21 09:41:26 -0500

    basic webpage

--- docs/index.js
+++ docs/index.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
     $.getJSON("tweaks.json", function (data) {
         console.log(data);
     });
+
+    $(".input-group-btn .dropdown-menu li a").click(function () {
+
+        var selText = $(this).html();
+
+        //working version - for single button //
+        //$('.btn:first-child').html(selText+'<span class="caret"></span>');  
+
+        //working version - for multiple buttons //
+        $(this).parents('.input-group-btn').find('.btn-search').html(selText);
+
+    });
 });
