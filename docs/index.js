fed9f6327211bb23bdd557723f4a7a1f179c59b7
jlippold <jlippold@datacation.com>
2018-03-20 07:35:14 -0400

    filter by working

--- docs/index.js
+++ docs/index.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             return {
                 data: {
                     searchTerm: "",
-                    filter: "",
+                    filter: (window.location.hash == "#cydia") ? "Working" : "",
                     isCydiaRequest: (window.location.hash == "#cydia"),
                     sort: "",
                     iOSVersionIndex: 0,
